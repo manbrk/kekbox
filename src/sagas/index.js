62a5e5d64878aa7acbf5dca0b09b53577c771b8e
manbrk <manbrk@gmail.com>
2022-11-19 14:20:37 +0200

    fake changes

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,7 +2,7 @@ import { takeLatest } from 'redux-saga/effects';
 import { COOL_ACTION } from '../actions/actionTypes';
 
 function* coolSaga() {
-  yield console.log('saga is OK -->');
+  yield console.log('saga is OK --> super ok!!!!');
 }
 
 export default function* saga() {
